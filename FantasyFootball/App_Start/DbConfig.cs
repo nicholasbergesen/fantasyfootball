6bd227e8c139edeb9ab49e348f9f06ca400a6470
Nicholas Bergesen <nicholasb.za@gmail.com>
2018-10-18 20:11:52 +0200

    Updated Fantasy football, test with live db backup

--- FantasyFootball/App_Start/DbConfig.cs
+++ FantasyFootball/App_Start/DbConfig.cs
@@ -17,17 +17,6 @@ namespace FantasyFootball
             db.Database.CreateIfNotExists();
 
             WebSecurity.InitializeDatabaseConnection("DefaultConnection", "WebUsers", "UserId", "UserName", autoCreateTables: true);
-            
-            if(!Roles.RoleExists("Admin"))
-                Roles.CreateRole("Admin");
-            if (!WebSecurity.UserExists("Bergie"))
-            {
-                WebSecurity.CreateUserAndAccount("Bergie", "910313");
-                db.UserProfiles.Add(new UserProfile() { UserName = "Bergie" });
-                db.UserPremierScores.Add(new UserPremierScore() { Score = 0, UserName = "Bergie" });
-            }
-            if(!Roles.IsUserInRole("Bergie","Admin"))
-                Roles.AddUserToRole("Bergie", "Admin");
 
             db.SaveChanges();
         }
